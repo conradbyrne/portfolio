bd585d2d0851022c934d94cb9ef0c56427da0f59
Conrad Byrne <dep9aj@virginia.edu>
2024-09-26 22:12:40 -0400

    fixed link issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://conradbyrne.github.io'
+      href='https://conradbyrne.github.io/portfolio'
       className='link footer__link'
     >
       Created By Conrad Byrne
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://conradbyrne.github.io',
+  homepage: 'https://conradbyrne.github.io/portfolio',
   title: 'Conrad Byrne',
 }
 
